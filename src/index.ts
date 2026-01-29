45367163cba96df572380a1f95fac3afcf0fe6cf
shevang5 <shevangpalgam@gmail.com>
2026-01-29 15:14:45 +0530

    implement security middleware

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,15 @@
 import express from "express";
 import subjectsRouter from "./routes/subjects.js";
 import cors from "cors";
+import securityMiddleware from "./middleware/security.js";
 const app = express();
 
 const PORT = process.env.PORT || 8000;
 
 app.use(express.json());
+
+app.use(securityMiddleware);
+
 app.use(cors({
     origin: true, // Allow any origin for debugging
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
